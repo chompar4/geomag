50e945a1559f0e8f92566fd1efee08a74e6d409a
Sam <sam.thompson@biarri.com>
2020-07-14 10:32:05 +1000

    new api repo

---
+++ app/main.py
@@ -0,0 +1,37 @@
+import datetime
+from json import dumps
+
+from flask import Flask, request
+from flask_restful import Api, Resource, abort
+from webargs import fields, validate
+from webargs.flaskparser import parser, use_kwargs
+
+from geomag import field as calculate_field
+
+app = Flask(__name__)
+api = Api(app)
+
+class GeomagField(Resource):
+
+    schema_args = {
+        "lat": fields.Float(required=True, validate=lambda dLat: -90 <= dLat <= 180), # accepts normalised [-90, 90] or unormalised [0, 180]
+        "lng": fields.Float(required=True, validate=lambda dLng: -180 <= dLng <= 360), # accepts normalised [-180, 180] or unormalised [0, 360]
+        "altitude_km": fields.Float(required=True, validate=lambda km: -1 < km <= 850),
+        "yr": fields.Integer(required=True, validate=lambda y: 2020 <= y < 2025),
+        "mth": fields.Integer(required=True, validate=lambda m: 1 <= m <= 12),
+        "day": fields.Integer(required=True, validate=lambda d: 1 <= d < 31)
+    }
+
+    @use_kwargs(schema_args)
+    def get(self, lat, lng, altitude_km, yr, mth, day):
+        result = calculate_field(lat, lng, altitude_km, datetime.date(yr, mth, day))
+        return result
+        
+api.add_resource(GeomagField, '/')
+
+@parser.error_handler
+def handle_request_parsing_error(err, req, schema, *, error_status_code, error_headers):
+    abort(error_status_code, errors=err.messages)
+
+if __name__ == '__main__':
+     app.run(port='5002', debug=True)
---
+++ runner.py
@@ -0,0 +1,100 @@
+import datetime
+import json
+
+import numpy as np
+
+from geomag import field
+
+if __name__ == "__main__":
+
+    # creates a json grid file of magnetic field in geographic coords
+
+    def create_json():
+
+        date = datetime.date(year=2020, month=1, day=1)
+
+        head = {
+                "discipline": 10,
+                "disciplineName": "Oceanographic_products",
+                "center": -3,
+                "centerName": "NOAA / British Geological Survey",
+                "refTime": "{}T00:00:00.000Z".format(date),
+                "significanceOfRT": 0,
+                "significanceOfRTName": "Analysis",
+                "parameterCategory": 1,
+                "parameterCategoryName": "Currents",
+                "parameterNumber": 2,
+                "parameterNumberName": "U_component_of_current",
+                "parameterUnit": "m.s-1",
+                "forecastTime": 0,
+                "surface1Type": 160,
+                "surface1TypeName": "Depth below sea level",
+                "surface1Value": 15,
+                "numberPoints": 10000,
+                "shape": 0,
+                "shapeName": "Earth spherical with radius = 6,367,470 m",
+                "scanMode": 0,
+                "nx": 100,
+                "ny": 100,
+                "lo1": 0,
+                "la1": 90,
+                "lo2": 360,
+                "la2": -90,
+                "dx": 3.6,
+                "dy": 1.8,
+            }
+
+        u = {"header": head, "data": []}
+        v = {"header": head, "data": []}
+
+        for dlat in np.linspace(
+            start=head["la1"], stop=head["la2"], num=head["nx"]
+        ):
+            print("u : lat {}".format(dlat))
+            for dlng in np.linspace(
+                start=head["lo1"], stop=head["lo2"], num=head["ny"]
+            ):
+
+                vals = field(dlat, dlng, date=date)
+
+                x = vals["X"] / 1
+                u["data"].append(x)
+
+                y = vals["Y"] / 1
+                v["data"].append(y)
+
+        to_dump = [v, u]
+
+        filename = "{}-0000-wind-gfs-1.0.json".format(date.day)
+
+        with open(filename, "w") as out:
+            json.dump(to_dump, out)
+
+        print("{} written".format(filename))
+
+
+def create_grib_cfgrib():
+
+    u = xr.DataArray(
+        np.zeros((181, 360)) + 300.,
+        coords=[
+            np.linspace(90., -90., 181),
+            np.linspace(0., 360., 360, endpoint=False),
+        ],
+        dims=['latitude', 'longitude'],
+    ).to_dataset(name='skin_temperature')
+
+    cfgrib.to_grib(u, 'out.grib')
+
+    import ipdb; ipdb.set_trace()
+
+class Bounds: 
+    def __init__(self, lat1, lat2, lng1, lng2):
+        self.lat1 = lat1
+        self.lat2 = lat2 
+        self.lng1 = lng1
+        self.lng2 = lng2
+
+if __name__ == "__main__":
+    create_json()
+    # create_grib_cfgrib()
