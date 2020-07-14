7be26a2c491ce165b83decf57df0f4fe5ae40264
Sam <sam.thompson@biarri.com>
2020-07-14 18:06:29 +1000

    contours plotting correctly, now to avoid closed loops

--- runner.py
+++ runner.py
@@ -3,47 +3,43 @@ import json
 from collections import defaultdict
 
 import geojsoncontour
+import matplotlib.pyplot as plt
 import numpy as np
 
 from geomag import field
 
-if __name__ == "__main__":
 
-    # creates a json grid file of magnetic field in geographic coords
+class Bounds: 
+    def __init__(self, lat1, lat2, lng1, lng2):
+        self.lat1 = lat1
+        self.lat2 = lat2 
+        self.lng1 = lng1
+        self.lng2 = lng2
 
 
-    date = datetime.date(year=2020, month=1, day=1)
+def create_file(day, mth, yr, bds): 
+
+    date = datetime.date(year=yr, month=mth, day=day)
 
     nx = 360
     ny = 181
 
-    lo1 = -179
-    lo2 = 179
-    la1 = 90
-    la2 = -90
+    x = np.linspace(start=bds.lat1, stop=bds.lat2, num=nx)
+    y = np.linspace(start=bds.lng1, stop=bds.lng2, num=ny)
 
-    x = np.linspace(start=la1, stop=la2, num=nx)
-    y = np.linspace(start=lo1, stop=lo2, num=ny)
-
-    lookup = []
-    for dlat in x:
-        print("u : lat {}".format(dlat))
-        row = [
+    lookup = [
+        [
             field(dlat, dlng, date=date)["D"]
             for dlng in y
         ]
-        lookup.append(row)
+        for dlat in x
+    ]
 
-
-    import matplotlib.pyplot as plt
     z = np.array(lookup)
     cs = plt.contour(
         y, x, z,
-        colors = 'black', 
-        linestyles = 'dashed', 
-        levels=range(-180, 180, 1)
+        levels=range(-180, 180, 2)
     )
-    plt.clabel(cs, fmt = '%.0f', inline = True)
     plt.savefig('plot.png')
 
     geojson = geojsoncontour.contour_to_geojson(
