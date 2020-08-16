ee703ef2d45d6e5f264af6fba3d8607d6048eec2
Sam Thompson <thompsonfilm@hotmail.com>
2020-08-16 10:26:54 +1000

    add build steps (#9)

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'geomag',
+    packages = ['geomag'],
+    version = '1.0',
+    description = 'earths magnetic field strength values and magnetic declination calculator',
+    author = 'Sam Thompson',
+    author_email = 'chompar4@gmail.com',
+    url = 'https://github.com/chompar4/geomag_api',
+    download_url = 'https://github.com/chompar4/geomag_api',
+)
